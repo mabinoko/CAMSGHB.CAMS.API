e8d65c33e77b67fcefd7a8273086200327ccc7e3
Adssadawut <adssadawut_cpe045@hotmail.com>
2019-09-28 09:51:07 +0700

    SampleData
    โค็ดตัวอย่าง

---
+++ CAMSGHB.CAMS.API/Enum/EnumMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAMSGHB.CAMS.API.Enum
+{
+    public class EnumMessage
+    {
+        public struct StatusMessage
+        {
+            public struct Success
+            {
+                public const string DataSaved = "ระบบทำการบันทึกข้อมูล เรียบร้อยแล้ว";
+
+                public const string DataSaveChange = "ระบบทำการบันทึกข้อมูลที่แก้ไข เรียบร้อยแล้ว";
+
+                public const string DataIsDeleted = "ระบบทำการลบข้อมูล เรียบร้อยแล้ว";
+            }
+
+        }
+    }
+}
